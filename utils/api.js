87cde4efda9c0d3b7145eeb7edee22a1a68d7292
Haris Smajlagic <info@harisco.net>
2021-08-16 14:56:39 +0200

    HTTPS for the OMDB api endpoint

--- utils/api.js
+++ utils/api.js
@@ -3,5 +3,5 @@
 export function generateOMDBSearchEndpoint(query, searchQueryFilters = {}, page = 1) {
   const yearFilter = searchQueryFilters.year ? `&y=${searchQueryFilters.year}` : '';
   const typeFilter = searchQueryFilters.type ? `&type=${searchQueryFilters.type}` : '';
-  return `http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}&s=${query}&page=${page}${yearFilter}${typeFilter}`;
+  return `https://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}&s=${query}&page=${page}${yearFilter}${typeFilter}`;
 }
